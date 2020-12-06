8fb49fa26d1ac6405e1166b3937f80cd291acc04
Logan <lcarpe21@uncc.edu>
2020-12-06 15:19:29 -0500

    Change

--- nccovid19/settings.py
+++ nccovid19/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'nccovid19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
