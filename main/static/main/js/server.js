8ce2ecf945b9ee3ca89823f745d2a1f7763af677
Levi <lbarrant@uncc.edu>
2020-12-09 19:29:39 -0500

    New version

--- main/static/main/js/server.js
+++ main/static/main/js/server.js
@@ -11,13 +11,13 @@ let url = 'mongodb://localhost:27017/budget_1';
 mongoose.set('useCreateIndex', true);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.jsonp());
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/',express.static('../public'));
 
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '../Covid-19/COVID_Fontend_files/front-end/index.html'));
+    response.sendFile(path.join(__dirname, '../Covid-19/COVID_Fontend_files/front-end/Index.html'));
 });
 
 app.listen(port, () => {
